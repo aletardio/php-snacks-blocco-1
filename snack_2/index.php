752baf729d97350a2b81a42ddc1989c4c1ab9ff0
aletardio <ale.tardio99@gmail.com>
2024-01-24 15:50:28 +0100

    Completed snack_2

--- snack_2/index.php
+++ snack_2/index.php
@@ -1,14 +1,20 @@
 <?php 
-
+    $resultClass = '';
     if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['age'])) {
 
         $name = $_GET['name'];
         $email = $_GET['email'];
         $age = $_GET['age'];
+
+        if (strlen($name) > 3 && filter_var($email,FILTER_VALIDATE_EMAIL) && is_numeric($age)) {
+            $resultClass = 'accesso-riuscito';
+            $resultMessage = "Accesso riuscito";
+        } else {
+            $resultClass = 'accesso-negato';
+            $resultMessage = "Accesso negato";
+        }
     }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,6 +22,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Snack_2</title>
 </head>
 <body>
@@ -42,7 +49,10 @@
                     </div>
                 </form>
             </div>
+            <div class="col-12 fs-5 fw-bold text-center">
+                <div class="<?php echo $resultClass; ?>"><?php echo $resultMessage; ?></div>
+            </div>
         </div>
-    </div>
+</div>
 </body>
 </html>
