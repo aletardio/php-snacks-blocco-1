e7187b805fb90f82fef75537ee2d2fc73d5321c3
aletardio <ale.tardio99@gmail.com>
2024-01-24 14:44:01 +0100

    first commit

---
+++ snack_1/index.php
@@ -0,0 +1,42 @@
+<?php 
+    $array_matches = [
+        [
+            'sq_home' => 'Olimpia Milano',
+            'sq_away' => 'Virtus Bologna',
+            'score_home' => 78,
+            'score_away' => 92,
+        ],
+        [
+            'sq_home' => 'Reyer Venezia',
+            'sq_away' => 'Nutribullet Trevios',
+            'score_home' => 102,
+            'score_away' => 89,
+        ],
+        [
+            'sq_home' => 'Dinamo Sassari',
+            'sq_away' => 'Fortitudo Bologna',
+            'score_home' => 98,
+            'score_away' => 86,
+        ],
+        [
+            'sq_home' => 'Happy Casa Brindisi',
+            'sq_away' => 'Openjobmetis Varese',
+            'score_home' => 83,
+            'score_away' => 90,
+        ],
+    ]
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    
+</body>
+</html>
